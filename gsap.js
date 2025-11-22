88c09452cad3e4c20ac1d1dfaa963802792f3723
Ayotheg <haywhyshotz@gmail.com>
2025-11-22 01:51:13 -0800

    add more files and code updates

---
+++ gsap.js
@@ -0,0 +1,16 @@
+// register TextPlugin if you haven't already
+gsap.registerPlugin(TextPlugin);
+
+const words = ["Brush", "Cream", "Bag", "Watch", "Stapler", "keys"];
+
+const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.5 });
+
+words.forEach(word => {
+    // replicate your original tween for each word
+    tl.to("#text-animate", {
+        duration: 3,
+        text: " " + word,
+        ease: "none",
+    });
+});
+
